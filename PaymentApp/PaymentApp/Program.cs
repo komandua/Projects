690bc10373c00ffd6c62110610c6c4ddd117ff41
Anantha <Anantha@ANANTHA-PC>
2017-01-22 17:03:02 -0500

    change 2

--- PaymentApp/PaymentApp/Program.cs
+++ PaymentApp/PaymentApp/Program.cs
@@ -14,6 +14,7 @@ namespace PaymentApp
         [STAThread]
         static void Main()
         {
+            // change 2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
